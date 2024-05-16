b190dd635a78a69d53269c857f5b538794729abe
lovertechie <sanketbarjora@gmail.com>
2024-05-16 23:34:55 +0530

    Update script.js
    Updated the javascript functionality to ensure it works both desktops and mobiles

--- script.js
+++ script.js
@@ -13,16 +13,20 @@ class Paper {
     currentPaperX = 0;
     currentPaperY = 0;
     rotating = false;
+
     init(paper) {
-        document.addEventListener('mousemove', (e) => {
+        const updatePosition = (e) => {
+            const clientX = e.clientX || e.touches[0].clientX;
+            const clientY = e.clientY || e.touches[0].clientY;
+
             if (!this.rotating) {
-                this.mouseX = e.clientX;
-                this.mouseY = e.clientY;
+                this.mouseX = clientX;
+                this.mouseY = clientY;
                 this.velX = this.mouseX - this.prevMouseX;
                 this.velY = this.mouseY - this.prevMouseY;
             }
-            const dirX = e.clientX - this.mouseTouchX;
-            const dirY = e.clientY - this.mouseTouchY;
+            const dirX = clientX - this.mouseTouchX;
+            const dirY = clientY - this.mouseTouchY;
             const dirLength = Math.sqrt(dirX * dirX + dirY * dirY);
             const dirNormalizedX = dirX / dirLength;
             const dirNormalizedY = dirY / dirLength;
@@ -41,35 +45,48 @@ class Paper {
                 this.prevMouseY = this.mouseY;
                 paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
             }
-        })
-        paper.addEventListener('mousedown', (e) => {
+        };
+
+        document.addEventListener('mousemove', updatePosition);
+        document.addEventListener('touchmove', updatePosition);
+
+        const startDragging = (e) => {
             if (this.holdingPaper) return;
             this.holdingPaper = true;
             paper.style.zIndex = highestZ;
             highestZ += 1;
-            if (e.button === 0) {
-                this.mouseTouchX = this.mouseX;
-                this.mouseTouchY = this.mouseY;
-                this.prevMouseX = this.mouseX;
-                this.prevMouseY = this.mouseY;
-            }
-            if (e.button === 2) {
+            const clientX = e.clientX || e.touches[0].clientX;
+            const clientY = e.clientY || e.touches[0].clientY;
+            this.mouseTouchX = clientX;
+            this.mouseTouchY = clientY;
+            this.prevMouseX = this.mouseTouchX;
+            this.prevMouseY = this.mouseTouchY;
+
+            if (e.type === 'mousedown' && e.button === 2) {
                 this.rotating = true;
+            } else if (e.type === 'touchstart') {
+                this.rotating = false;
             }
-        });
-        window.addEventListener('mouseup', () => {
+        };
+
+        paper.addEventListener('mousedown', startDragging);
+        paper.addEventListener('touchstart', startDragging);
+
+        const stopDragging = () => {
             this.holdingPaper = false;
             this.rotating = false;
-        });
+        };
+
+        window.addEventListener('mouseup', stopDragging);
+        window.addEventListener('touchend', stopDragging);
     }
 }
+
 const papers = Array.from(document.querySelectorAll('.paper'));
 papers.forEach(paper => {
     const p = new Paper();
     p.init(paper);
 });
-
-
 
 const noButton = document.getElementById("no-btn");
 const yesButton = document.getElementById("yes-btn");
